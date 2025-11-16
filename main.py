0a319ca445c17d5b6477e91ae00eb479d3c25313
Alex Silver <asilver@hmc.edu>
2025-11-16 12:33:43 -0800

    need to work on sending but simple features work

--- main.py
+++ main.py
@@ -1,46 +1,90 @@
-import socket
 import threading
-from router.py import Router
+from router import Router
 
-def get_port_id_from_topology(topology_file):
-    with open(topology_file, 'r') as f:
-        lines = f.readlines()
-        mylines = lines[2].split()
-        port = int(mylines[2])
-        server_id = int(mylines[1])
-    return port, server_id
+# def get_port_id_from_topology(topology_file):
+#     with open(topology_file, 'r') as f:
+#         lines = f.readlines()
+#         mylines = lines[2].split()
+#         port = int(mylines[2])
+#         server_id = int(mylines[1])
+#     return port, server_id
 
+def initialize_router(filepath):
+    myrouter = Router()
+    myrouter.open_topology_file(filepath)
+    # myrouter.set_server_port() # TODO: this should throw an error or smth if run too early
+    myrouter.build_routing_table()
+    return myrouter
 
-def update_cost(router, neighbor_id, new_cost):
-    pass
+def handle_connection(socket):
+    while True:
+        # use recvfrom in udp socket
+        data, address = socket.recvfrom(1024)
+        # decode the received data to string and print out
+        message = data.decode()
 
-def user_input_handler():
+        #check for updates
+
+        
+
+def user_input_handler(myrouter):
 
     while(True):
         user_input = input("Enter command: ")
 
-        if user_input[0:5] == "update":
+        if user_input[0:6] == "update":
+            # <server1-id> <server1-id> <new_cost>
             args = user_input.split()
-            update_cost(router, int(args[1]), int(args[2]))
-            pass
+        # try:
+            myrouter.update_cost(int(args[1]), int(args[3]))
+        # except:
+        #     print("update Error updating cost.")
+        #     continue
+            print("update SUCCESS")
 
         elif user_input == "step":
-            pass
+        #try:
+            myrouter.send_routing_update()
+        #except:
+            # print("step Error sending router update.")
+            # continue
+            print("step SUCCESS")
 
         elif user_input == "packets":
-            pass
+            try:
+                packets = myrouter.get_num_packets_sent()
+                print("Total packets sent: ", packets)
+            except:
+                print("packets Error retrieving packet count.")
+                continue
+            print("packets SUCCESS")
 
         elif user_input == "display":
-            pass
+            try:
+                myrouter.display()
+            except:
+                print("display Error displaying routing table.")
+                continue
+            print("display SUCCESS")
 
         elif user_input[0:6] == "disable":
-            pass
+            args = user_input.split()
+            try:
+                myrouter.remove_route(int(args[1]))
+            except:
+                print("disable Error disabling link.")
+                continue
+            print("disable SUCCESS")
 
         elif user_input == "crash":
-            pass
-
-        elif user_input == "exit":
-            break
+            try:
+                for i in myrouter.num_neighbors:
+                    myrouter.remove_route(myrouter.neighbor_ids[i])
+            except:
+                print("crash Error crashing router.")
+                continue
+            print("crash SUCCESS")
+            exit()
 
         else:
             print("Invalid input. Please try again.")
@@ -49,25 +93,37 @@ def user_input_handler():
 
 def main():
 
+   
+
     # Get server configuration from user
     while(True):
         server_input = input("Please enter server configuration.").split()
         if server_input[0] == "server":
             topology_file = server_input[2]
             routing_update_interval = int(server_input[4])
-            port,server_id = get_port_id_from_topology(topology_file)
             break
+        
         else: 
             print("Invalid input. Please try again.")
             continue
 
-    # Creat router and start router thread
-    router = Router("topology1.txt")
-    router_thread = threading.Thread(target=router.run)
-    router_thread.start()
+    myrouter = initialize_router(topology_file)
     
+    # Start listening thread
+    listening_thread = threading.Thread(target=handle_connection, args=(myrouter.listening_socket,))
+    listening_thread.start()
 
     # Start user input thread
-    input_thread = threading.Thread(target=input_handler)
+    input_thread = threading.Thread(target=user_input_handler, args=(myrouter,))
     input_thread.start()
-    input_thread.join()
+
+    while(input_thread.is_alive()):
+        pass
+    exit()
+
+main()
+
+
+
+# Example command to run the server with topology file and router ID
+# server -t topology1.txt -i 3
--- router.py
+++ router.py
@@ -1,16 +1,18 @@
 import sys
+import socket
 
 class Router:
-    def __init__(self, router_id):
-        self.router_id = router_id
+    def __init__(self):
+        self.router_id = None
         self.routing_table = {}
         self.cost_table = {} # mapping neighbor IDs to integer costs
         self.neighbor_ip_port_table = {} # mapping neighbor IDs to (IP, port) tuples
         self.server_port = None
+        self.listening_socket = None
         self.num_neighbors = 0
 
-    def set_server_port(self, port):
-        self.server_port = port
+    def set_server_port(self):
+        self.server_port = self.neighbor_ip_port_table[self.router_id][1]
 
     def get_cost(self, neighbor_id):
         # returns cost if neighbor exists, else infinity
@@ -25,11 +27,17 @@ class Router:
         # topology is a plain text file mapping neighbor IDs to costs
         lines = topology.strip().split('\n')
 
+        self.router_id = int(lines[-1].split()[0]) # second line is this router's ID
+
         self.num_neighbors = int(lines[0]) - 1
 
         for i in range(2, 3 + self.num_neighbors):
             neighbor_id, neighbor_ip, neighbor_port = lines[i].split()
             self.neighbor_ip_port_table[int(neighbor_id)] = (neighbor_ip, int(neighbor_port))
+
+        self.server_port = self.neighbor_ip_port_table[self.router_id][1]
+        self.listening_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.listening_socket.bind(("", self.server_port))
 
         for i in lines[self.num_neighbors + 3:]: # start reading edges after global vars
             server_id, neighbor_id, cost = i.split()
@@ -38,6 +46,8 @@ class Router:
 
             if int(neighbor_id) == self.router_id:
                 raise ValueError("Topology file should not set self as neighbor")
+            
+
 
         # Format of topology.txt:
             # num total routers
@@ -62,10 +72,15 @@ class Router:
             self.routing_table[neighbor_id]["ip, port"] = (self.neighbor_ip_port_table[neighbor_id])
             self.routing_table[neighbor_id]["next_hop"] = neighbor_id
             self.routing_table[neighbor_id]["cost"] = self.cost_table[neighbor_id]
-            
+
+    def update_routing_table(self):
+        for neighbor_id in self.cost_table.keys():
+            self.routing_table[neighbor_id]["cost"] = self.cost_table[neighbor_id]
+
     def update_cost(self, neighbor_id, new_cost):
         if neighbor_id in self.cost_table:
             self.cost_table[neighbor_id] = new_cost
+            self.update_routing_table()
         else:
             print(f"Neighbor {neighbor_id} not found.")
 
@@ -76,6 +91,61 @@ class Router:
             next_hop = self.routing_table[destination]["next_hop"]
             cost = self.routing_table[destination]["cost"]
             print(f"{destination}\t\t{next_hop}\t\t{cost}")
+
+    # def connect_to_neighbor(self, neighbor_id):
+    #     # Using sockets
+    #     if neighbor_id in self.neighbor_ip_port_table:
+    #         ip, port = self.neighbor_ip_port_table[neighbor_id]
+    #         print(f"Connecting to neighbor {neighbor_id} at {ip}:{port}")
+    #         # Here you would add the actual socket connection code
+    #         s = socket.socket()
+    #         s.connect((ip, port))
+    #         return s
+
+    #     else:
+    #         print(f"Neighbor {neighbor_id} not found.")
+
+    def send_single_update(self, neighbor_id, dv):
+        # sends DV to a single neighbor
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        while True:
+            # use sendto in udp socket using neighbor IP, port
+            dv = {}
+            for dest_id in self.routing_table.keys():
+                dv[dest_id] = self.routing_table[dest_id]["cost"]
+            s.sendto(dv.encode())
+            # use recvfrom in udp socket
+            s.close()
+
+    def receive_single_update(self, connection):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        while True:
+            data = connection.recv(1024).decode()
+            print(f"Received distance vector update: {data}")
+            # data = data.split() # neighbor id, cost
+            # self.cost_table[data[0]] = int(data[1])
+            # print(f"New distance from {data[0]}: {data[1]}")
+            return
+
+    def send_distance_updates(self):
+        # Sends distance updates to all neighbors
+        print("Sending distance vector update to neighbors...")
+        for neighbor_id in self.cost_table.keys():
+            # sock = self.connect_to_neighbor(neighbor_id)
+            # sock.send((str(neighbor_id), str(self.cost_table[neighbor_id])).encode())
+            # print(f"Sent distance {self.cost_table[neighbor_id]} to neighbor {neighbor_id}")
+            # return
+            self.send_single_update(neighbor_id, str(self.cost_table[neighbor_id]))  
+        
+    # def handle_distance_update(self, conn, addr):
+    #     # connection socket and address
+    #     # Placeholder for handling incoming connections
+    #     print(f"Handling connection from {addr}")
+    #     data = conn.recv(1024).decode()
+    #     data = data.split() # neighbor id, cost
+    #     self.cost_table[data[0]] = int(data[1])
+    #     print(f"New distance from {data[0]}: {data[1]}")
+    #     return
 
 
     # def add_route(self, destination, next_hop):
@@ -91,12 +161,40 @@ class Router:
     # def __str__(self):
     #     return f"Router {self.router_id} Routing Table: {self.routing_table}"
 
+    def bellman_ford_update(self, neighbor_id, neighbor_dv):
+        # Implement the Bellman-Ford algorithm to update routing table
+        # neighbor_dv is a dict mapping destination IDs to costs from neighbor_id
+        update = False
+        dist_to_neighbor = neighbor_dv[self.router_id]
+
+        ## Neighbor DV: {1: cost, 2: cost, ...}
+        
+        for dest_id in neighbor_dv.keys():
+            # distance to dest_id without going through neighbor
+            current_cost = self.cost_table.get(dest_id, float('inf'))
+
+            # distance to dest_id via neighbor
+            cost_via_neighbor = neighbor_dv[self.router_id] + neighbor_dv[dest_id]
+
+            if cost_via_neighbor < current_cost:
+                self.cost_table[dest_id] = cost_via_neighbor
+                self.routing_table[dest_id] = {
+                    "next_hop": neighbor_id,
+                    "cost": cost_via_neighbor,
+                    "ip, port": self.neighbor_ip_port_table.get(neighbor_id, ("", 0))
+                }
+                update = True\
+            
+            if update:
+                print(f"Routing table to {dest_id} updated via neighbor {neighbor_id} from cost {current_cost} to {cost_via_neighbor}")
+
+          
 def main(): # run python3 router.py <router_id> <topology_file_path>
     new_router = Router(int(sys.argv[1]))
     print("Made new router with ID ", new_router.router_id)
     new_router.open_topology_file(sys.argv[2])
-    # print("Cost table: ", new_router.cost_table)
-    # print("IP-Port table: ", new_router.neighbor_ip_port_table)
+    print("Cost table: ", new_router.cost_table)
+    print("IP-Port table: ", new_router.neighbor_ip_port_table)
     new_router.build_routing_table()
     # print("Router's updated routing table: ", new_router.routing_table)
     new_router.display()
